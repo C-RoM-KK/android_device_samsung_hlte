a462d669e06fccdc9390dc3b313d06848416643d
aways <aways@c-rom.org>
2014-06-20 05:34:59 +0200

    set it for new vendor

--- crom.mk
+++ crom.mk
@@ -1,14 +1,13 @@
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
-# Inherit some common C-RoM stuff.
-$(call inherit-product, vendor/crom/config/common_gsm.mk)
+# Enhanced NFC
+$(call inherit-product, vendor/crom/config/nfc_enhanced.mk)
 
 # Inherit some common C-RoM stuff.
 $(call inherit-product, vendor/crom/config/common_phone.mk)
 
-# bootanimation
-PRODUCT_COPY_FILES += \
-    vendor/crom/prebuilt/common/media/xxhdpi/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
+# Inherit multicarrier.
+$(call inherit-product, vendor/crom/config/common_multicarrier.mk)
 
 PRODUCT_DEVICE := hlte
 PRODUCT_NAME := crom_hlte
